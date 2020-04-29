d061c2e65eabad0545755895e9a73c03a4a7d312
abrantes <abrantes.nsc@gmail.com>
2020-04-29 17:21:16 -0300

    edição do template

--- blog/website/views.py
+++ blog/website/views.py
@@ -3,7 +3,7 @@ from .models import Post
 
 
 def hello_blog(request):
-    list_posts = Post.objects.all()
+    list_posts = Post.objects.filter(deleted=False)
     data = {'name': 'Curso de Django3',
             'posts': list_posts }
 
